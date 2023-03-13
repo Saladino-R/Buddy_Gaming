8c7827b9715729af5fe26b476c85f8a56db806ad
guicaron <guicaron0201@gmail.com>
2023-03-13 09:20:35 +0100

    bug bug

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,6 +4,9 @@ import "./controllers"
 import "bootstrap"
 import { selectGame } from "./components/_select_game";
 
+//Swiper J.S for the carousel
+
+
 //FOR NEW FRIEND REQUEST: to do a POST with with a link_to(usually only for GETs)
 import { Turbo } from "@hotwired/turbo-rails"
 Turbo.session.drive = false
