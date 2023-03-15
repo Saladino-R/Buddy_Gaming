8f9f0ca589e59c5acad20cd75e61d53ec53cdcf2
guicaron <guicaron0201@gmail.com>
2023-03-15 18:50:19 +0100

    start of carousel

--- app/javascript/components/_swiper_game.js
+++ app/javascript/components/_swiper_game.js
@@ -1,3 +1,20 @@
 export const swiperGame = () => {
-
+  const swiperContainer = document.querySelector('.swiper-container')
+  if (swiperContainer){
+    const swiper = new Swiper('.swiper-container', {
+      pagination: '.swiper-pagination',
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflow: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows : true
+      },
+      loop: true
+    });
+  }
 }
