bdd9f39bc38f3b6d1c3e33c1457903824a0867a4
guicaron <guicaron0201@gmail.com>
2023-03-17 11:10:24 +0100

    laser

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module BuddyGaming
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.assets.precompile << "audios/*"
+
 
     # Configuration for the application, engines, and railties goes here.
     #
